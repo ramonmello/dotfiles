ef490e8887469500c1f5caa0b64b8e3090d98099
Ramon Mello <ramonomello@gmail.com>
2023-12-08 01:44:14 -0300

    feat: add atuin to fish settings

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,4 @@
+
 # aliases
 alias la "ls -A"
 alias g git
@@ -7,5 +8,9 @@ alias lzg lazygit
 set fish_greeting
 
 eval "$(/opt/homebrew/bin/brew shellenv)"
+
+if status is-interactive
+    atuin init fish | source
+end
 
 starship init fish | source
