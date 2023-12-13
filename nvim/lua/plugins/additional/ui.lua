04f249f9b9c75a5bf481889a405b72caca51eca2
Ramon Mello <ramonomello@gmail.com>
2023-12-13 16:38:23 -0300

    feat: enable colorizer on startup

--- nvim/lua/plugins/additional/ui.lua
+++ nvim/lua/plugins/additional/ui.lua
@@ -1,4 +1,13 @@
 return {
   -- Color highlighter for hex codes
-  { "norcalli/nvim-colorizer.lua" },
+  {
+    "norcalli/nvim-colorizer.lua",
+    config = function(_, opts)
+      require("colorizer").setup(opts)
+
+      vim.defer_fn(function()
+        require("colorizer").attach_to_buffer(0)
+      end, 0)
+    end,
+  },
 }
