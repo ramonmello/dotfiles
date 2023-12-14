8e703a2091bc90009f7d54ae607ffd3ef84abdbe
Ramon Mello <ramonomello@gmail.com>
2023-12-14 02:46:57 -0300

    feat: improve theme settings

--- nvim/lua/plugins/additional/colorscheme.lua
+++ nvim/lua/plugins/additional/colorscheme.lua
@@ -1,15 +1,50 @@
 return {
   {
     "rebelot/kanagawa.nvim",
-    lazy = true,
-    opts = {
-      transparent = true,
-    },
-    -- With this settings the transparent background is not working
-    -- lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    -- priority = 1000, -- make sure to load this before all the other start plugins
-    -- config = function()
-    --   vim.cmd([[colorscheme kanagawa-dragon]])
-    -- end,
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("kanagawa").setup({
+        theme = "dragon",
+        transparent = true,
+        colors = {
+          theme = {
+            all = {
+              ui = {
+                bg_gutter = "none",
+              },
+            },
+          },
+        },
+        overrides = function(colors)
+          local theme = colors.theme
+          return {
+            NormalFloat = { bg = "none" },
+            FloatBorder = { bg = "none" },
+            FloatTitle = { bg = "none" },
+
+            NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
+
+            LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+            MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+
+            -- Telescope settings
+            TelescopeTitle = { fg = theme.ui.special, bold = false },
+            TelescopePromptNormal = { bg = theme.ui.bg_p1 },
+            TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
+            TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 },
+            TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },
+            TelescopePreviewNormal = { bg = theme.ui.bg_dim },
+            TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
+
+            -- Popup menu settings
+            Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 }, -- add `blend = vim.o.pumblend` to enable transparency
+            PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
+            PmenuSbar = { bg = theme.ui.bg_m1 },
+            PmenuThumb = { bg = theme.ui.bg_p2 },
+          }
+        end,
+      })
+    end,
   },
 }
