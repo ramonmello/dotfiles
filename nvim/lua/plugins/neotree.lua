760e6633a948b9583d7613e89b1e8a11e2e56d6c
Ramon Mello <ramonomello@gmail.com>
2024-07-01 02:46:10 -0300

    feat: change neotree position

--- nvim/lua/plugins/neotree.lua
+++ nvim/lua/plugins/neotree.lua
@@ -1,13 +1,27 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = {
+    popup_border_style = "rounded",
     window = {
-      position = "right",
+      position = "float",
       mappings = {
         ["Y"] = "none",
       },
     },
     filesystem = {
+      window = {
+        popup = {
+          position = { col = "100%", row = "2" },
+          size = function(state)
+            local root_name = vim.fn.fnamemodify(state.path, ":~")
+            local root_len = string.len(root_name) + 4
+            return {
+              width = math.max(root_len, 50),
+              height = vim.o.lines - 6,
+            }
+          end,
+        },
+      },
       filtered_items = {
         hide_dotfiles = false,
         hide_by_name = {
